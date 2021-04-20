fb1053f256675add9ea5fdfd2b57c7b0ab296811
Dmitry Christie <dmitry.christie@gmail.com>
2021-04-20 13:54:49 +0200

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -1116,9 +1116,9 @@ if($featured_img_url) {
         <div class="as-seen-outer" data-aos="fade-up">
             <div class="as-seen container">
                 
-                     <?php if( have_rows('featured_on', 'options') ): ?>
+                     <?php if( have_rows('featured_on') ): ?>
                                     
-                                        <?php while ( have_rows('featured_on', 'options') ) : the_row(); ?>
+                                        <?php while ( have_rows('featured_on') ) : the_row(); ?>
                                              <div class="source">
                                                  <img src="<?php $icon = get_sub_field('logo'); echo $icon; ?>">
 
